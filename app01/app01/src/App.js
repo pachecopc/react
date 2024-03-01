d495f8b398e11f8fa7051d833da066f81e956c5b
Paulo <120028058+pachecopc@users.noreply.github.com>
2024-03-01 10:59:16 -0300

    atualizado

--- app01/app01/src/App.js
+++ app01/app01/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Logo from './componentes/imgs/noite.jpg'
+import Tarde from './componentes/imgs/tarde.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App(){
+  const canal=()=>{
+    return ('CFB Cursos')
+  }
+  function curso(){
+    return "Curso React"
+  }
+ return (
+  <section>
+    <p>{'Canal: ' + canal()}</p>
+    <p>{curso()}</p>
+    <img src={Logo}></img>
+    <img src={Tarde}></img>
+  </section>
+ )
 }
 
-export default App;
