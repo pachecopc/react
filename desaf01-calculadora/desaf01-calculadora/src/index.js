ec6c1f208821a05ad95eb5182abdefcb995085d1
Paulo <120028058+pachecopc@users.noreply.github.com>
2024-03-03 19:50:10 -0300

    atualizado

--- desaf01-calculadora/desaf01-calculadora/src/index.js
+++ desaf01-calculadora/desaf01-calculadora/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Calculator from './main/Ccalculator';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-import './index.css';
+import GlobalStyles from './global';
 
-const elemento = document.getElementById('root');
-ReactDom.render(
-  <div>
-    <h1>Calculadora</h1>
-    <Calculator/>
-  </div>,
-  elemento);
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>
+);
 
