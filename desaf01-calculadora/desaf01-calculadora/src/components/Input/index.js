34338bb9313dde32a6fc45d689b3ff48bae74f2d
Paulo <120028058+pachecopc@users.noreply.github.com>
2024-03-03 23:16:52 -0300

    atualmente

--- desaf01-calculadora/desaf01-calculadora/src/components/Input/index.js
+++ desaf01-calculadora/desaf01-calculadora/src/components/Input/index.js
@@ -1,12 +1,1 @@
 
-import { InputContainer } from './styles';
-
-const Input = ({value}) => {
-    return (
-      <InputContainer>
-       <input disabled value={value}/>
-      </InputContainer>
-    );
-  }
-  
-  export default Input;
